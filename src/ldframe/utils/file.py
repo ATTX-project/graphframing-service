0521040aaf3a9e6069ef2b58c6d61efce420dbf6
blankdots <stefan@blankdots.com>
2017-11-24 11:05:22 +0200

    file path URI

--- src/ldframe/utils/file.py
+++ src/ldframe/utils/file.py
@@ -8,7 +8,7 @@ data = {'directory': os.environ['DATADIR'] if 'DATADIR' in os.environ else "/att
 def results_path(content, extension):
     """Write results to specific file."""
     try:
-        path = "{0}/graphframing/{1}".format(data["directory"], uuid.uuid4().hex)
+        path = "file://{0}/graphframing/{1}".format(data["directory"], uuid.uuid4().hex)
         if not os.path.exists(path):
             os.makedirs(path)
         full_path = "{0}/{1}.{2}".format(path, uuid.uuid1().hex, extension)
